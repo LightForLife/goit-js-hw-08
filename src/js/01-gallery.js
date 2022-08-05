c30fe5dba6424865544f6c711d3f4672c3a6662d
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2022-08-05 16:10:47 +0300

    add task-01 and task-02

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+// Ссылка на ul.gallery
+const galleryList = document.querySelector('.gallery');
 
-console.log(galleryItems);
+// Создаем разметку для изображений
+const galleryItem = makeItemsMarkup(galleryItems);
+
+function makeItemsMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+  </a>`;
+    })
+    .join('');
+}
+
+// Добавляем изображение в ul.gallery
+galleryList.insertAdjacentHTML('beforeend', galleryItem);
+
+// Подключаем библиотеку SimpleLightbox
+const gallery = new SimpleLightbox('.gallery a', {
+  // Добавляем подпись с атрибута alt
+  captionsData: 'alt',
+  // Подпись появляется через 250ms
+  captionDelay: '250',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+// ключ к текущему времени воспроизведения
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+// сохраняем время воспроизведения в локальное хранилище
+const onTimeUpdate = function ({ seconds }) {
+  localStorage.setItem(CURRENT_TIME_KEY, seconds);
+};
+
+// вызываем метод on() с событием onTimeUpdate и ставим throttle 1 секунду
+player.on('timeupdate', throttle(onTimeUpdate, 1000));
+
+// передаем текущее время с хранилища
+
+let savedTime = localStorage.getItem(CURRENT_TIME_KEY);
+
+if (savedTime) {
+  // console.log(savedTime);
+} else savedTime = 0;
+
+player.setCurrentTime(savedTime);
